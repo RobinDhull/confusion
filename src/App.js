df81433f4093777d3b27374aa4db555f2bd77408
Robin Dhull <rsdhull707@gmail.com>
2020-05-20 22:44:57 +0530

    Added assets folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
-
 export default App;
